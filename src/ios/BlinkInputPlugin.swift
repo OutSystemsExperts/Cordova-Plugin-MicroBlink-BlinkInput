0f74b36fd5e6a7f0c640b59ea395db30ae3b8779
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-07-12 14:45:12 +0100

    Remove .framework and .bundle. Remove ViewController and pass that logic to plugin Add NSCameraUsageDescription to Info.plist

--- src/ios/BlinkInputPlugin.swift
+++ src/ios/BlinkInputPlugin.swift
@@ -4,7 +4,7 @@ import MicroBlink
 @objcMembers
 class BlinkInputPlugin : CDVPlugin {
     
-    private var scanViewController: ViewController?
+    private var callback : CDVInvokedUrlCommand?
 
     func initSDK(_ command:CDVInvokedUrlCommand) {
         let license:String = command.arguments[0] as? String ?? ""
@@ -25,12 +25,61 @@ class BlinkInputPlugin : CDVPlugin {
     }
     
     func chequeOCR(_ command:CDVInvokedUrlCommand) {
-        let scanViewController = ViewController()
-    
-        
         DispatchQueue.main.async {
-            scanViewController.startScanTapped(self.viewController)
+            self.callback = command
+            // Create MBFieldByFieldOverlaySettings
+            let settings = MBFieldByFieldOverlaySettings(scanElements: MBGenericPreset.getPreset()!)
+            
+            // Create field by field VC
+            let fieldByFieldVC = MBFieldByFieldOverlayViewController(settings: settings, delegate: self)
+            
+            // Create scanning VC
+            let recognizerRunnerViewController: (UIViewController & MBRecognizerRunnerViewController)? = MBViewControllerFactory.recognizerRunnerViewController(withOverlayViewController: fieldByFieldVC)
+            
+            
+            // Present VC
+            self.viewController.present(recognizerRunnerViewController!, animated: true, completion: nil)
+            //scanViewController.startScanTapped(self.viewController)
         }
     }
     
+}
+
+extension BlinkInputPlugin : MBFieldByFieldOverlayViewControllerDelegate {
+    func field(_ fieldByFieldOverlayViewController: MBFieldByFieldOverlayViewController, didFinishScanningWith scanElements: [MBScanElement]) {
+        
+        fieldByFieldOverlayViewController.recognizerRunnerViewController?.pauseScanning()
+        
+        var dict = [String: String]()
+        for element: MBScanElement in scanElements {
+            if (element.scanned) {
+                dict[element.identifier] = element.value
+            }
+        }
+        
+        var result = [String : Any]()
+        for (key, value) in dict {
+            result.updateValue("\(value)", forKey: "\(key)" )
+        }
+        result.updateValue(true, forKey: "success")
+        
+        //let result: [String: Any] = ["success": true, "granted": true]
+        let pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: result)
+        self.commandDelegate!.send(
+            pluginResult,
+            callbackId: self.callback?.callbackId
+        )
+        
+        //let alert = UIAlertController(title: "Field by field Result", message: "\(description)", preferredStyle: UIAlertController.Style.alert)
+        //alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        //    self.viewController.dismiss(animated: true, completion: nil)
+        //}))
+        
+        //fieldByFieldOverlayViewController.present(alert, animated: true, completion: nil)
+        self.viewController.dismiss(animated: true, completion: nil)
+    }
+    
+    func field(byFieldOverlayViewControllerWillClose fieldByFieldOverlayViewController: MBFieldByFieldOverlayViewController) {
+        self.viewController.dismiss(animated: true, completion: nil)
+    }
 }
