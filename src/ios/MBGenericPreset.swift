f3d5a81dbacbc8f12a28b332c48204a65b651b52
Bruno Azevedo <bruno.j.azevedo.94@gmail.com>
2019-07-12 14:59:46 +0100

    Fix OCR elements identifiers

--- src/ios/MBGenericPreset.swift
+++ src/ios/MBGenericPreset.swift
@@ -6,17 +6,17 @@ class MBGenericPreset {
     class func getPreset() -> [MBScanElement]? {
         var scanElements = [MBScanElement]()
         
-        let priceElement = MBScanElement(identifier: "Amount", parser: MBAmountParser())
+        let priceElement = MBScanElement(identifier: "amount", parser: MBAmountParser())
         priceElement.localizedTitle = "Amount"
         priceElement.localizedTooltip = "Scan amount"
         scanElements.append(priceElement)
         
-        let dateElement = MBScanElement(identifier: "Date", parser: MBDateParser())
+        let dateElement = MBScanElement(identifier: "date", parser: MBDateParser())
         dateElement.localizedTitle = "Date"
         dateElement.localizedTooltip = "Scan date"
         scanElements.append(dateElement)
         
-        let nameElement = MBScanElement(identifier: "Pay to Name", parser: MBRegexParser(regex: "[A-Za-z ]+"))
+        let nameElement = MBScanElement(identifier: "payTo", parser: MBRegexParser(regex: "[A-Za-z ]+"))
         nameElement.localizedTitle = "Pay to Name"
         nameElement.localizedTooltip = "Scan pay to full name"
         scanElements.append(nameElement)
